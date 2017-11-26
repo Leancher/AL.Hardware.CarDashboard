fcda9714a17cf238a0056c7a7d4bc998e13c95da
Zarkon <leancher@yandex.ru>
2017-11-26 07:46:04 +0300

    (no message)

---
+++ AL.Software.CarDashboard/AppForm.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AppForm
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btRev = New System.Windows.Forms.Button()
+        Me.btForw = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btRev
+        '
+        Me.btRev.Location = New System.Drawing.Point(87, 389)
+        Me.btRev.Name = "btRev"
+        Me.btRev.Size = New System.Drawing.Size(75, 23)
+        Me.btRev.TabIndex = 1
+        Me.btRev.Text = "<"
+        Me.btRev.UseVisualStyleBackColor = True
+        '
+        'btForw
+        '
+        Me.btForw.Location = New System.Drawing.Point(184, 389)
+        Me.btForw.Name = "btForw"
+        Me.btForw.Size = New System.Drawing.Size(75, 23)
+        Me.btForw.TabIndex = 2
+        Me.btForw.Text = ">"
+        Me.btForw.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Dashboard.My.Resources.Resources.SpeedScale
+        Me.PictureBox1.Location = New System.Drawing.Point(58, 10)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(688, 180)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(688, 485)
+        Me.Controls.Add(Me.btForw)
+        Me.Controls.Add(Me.btRev)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btRev As Button
+    Friend WithEvents btForw As Button
+End Class
---
+++ AL.Software.CarDashboard/AppForm.vb
@@ -0,0 +1,65 @@
+﻿Public Class AppForm
+    Dim Graph1 As Graphics
+    Dim Pen1 As New Pen(Color.Orange, 20)
+    Dim Xarc, Yarc, angle1, angle2, AngleStep As Single
+    Dim Speed, Radius, AngleStart, AngleSweep, MaxSpeed As Integer
+
+    Private Sub AppInit()
+        Radius = 590
+        AngleStart = 300
+        AngleSweep = 60
+        MaxSpeed = 180
+        AngleStep = 0.333
+        Xarc = -246.5
+        Yarc = 19.5
+    End Sub
+
+    Private Sub PictureBoxInit()
+        Graph1.Clear(Color.FromArgb(64, 64, 64))
+        Graph1.DrawImage(My.Resources.SpeedScale, 0, 0)
+    End Sub
+
+    Private Sub DrawArc()
+        angle1 = AngleStart - (MaxSpeed - Speed) * AngleStep
+        angle2 = -(AngleSweep - (MaxSpeed - Speed) * AngleStep)
+        Graph1.DrawArc(Pen1, Xarc, Yarc, Radius * 2, Radius * 2, angle1, angle2)
+        'Graph1.DrawArc(Pen1, x0, y0, radius * 2, radius * 2, 300, -60)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btForw.Click
+        PictureBoxInit()
+        Graph1.DrawArc(Pen1, Xarc, Yarc, Radius * 2, Radius * 2, AngleStart, -AngleSweep)
+    End Sub
+
+    Private Sub btForw_Click(sender As Object, e As EventArgs) Handles btForw.Click
+        PictureBoxInit()
+        Speed = Speed + 5
+        If Speed > 180 Then Speed = 180
+        DrawArc()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AppInit()
+
+    End Sub
+
+    Private Sub btRev_Click(sender As Object, e As EventArgs) Handles btRev.Click
+        PictureBoxInit()
+        Speed = Speed - 5
+        If Speed < 0 Then Speed = 0
+        DrawArc()
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Graph1 = PictureBox1.CreateGraphics()
+        'PictureBoxInit()
+    End Sub
+End Class
+'Движение стрелки по кругу
+'Const radian = (22 / 7) / 180
+'Dim a, b
+'a = radius * Math.Cos(angle * radian)
+'b = radius * Math.Sin(angle * radian)
+'x = x0 + b
+'y = y0 - a
+'Graph1.DrawLine(Pen1, x0, y0, x, y)
